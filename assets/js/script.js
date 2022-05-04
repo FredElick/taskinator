a5b09f1c66dddef63c17291a28dba3bf53cb379a
Fred <felick4000@gmail.com>
2022-05-04 18:21:51 -0400

    cleaned up functions

---
+++ assets/js/script.js
@@ -0,0 +1,34 @@
+var formEl= document.querySelector("#task-form")
+var tasksToDoEl=document.querySelector("#tasks-to-do");
+var taskFormHandler= function(event){
+
+    event.preventDefault();
+    var taskNameInput=document.querySelector("input[name='task-name']").value;
+    console.log(taskNameInput);
+    var taskTypeInput=document.querySelector("select[name='task-type']").value;
+    console.log(taskTypeInput);
+    var taskDataObj={
+        name: taskNameInput,
+        type: taskTypeInput
+    };
+    if(!taskNameInput || !taskTypeInput){
+        alert("you need to fill out the task form!");
+        return false;
+    }
+    formEl.reset();
+    createTaskEl(taskDataObj);
+}
+
+var createTaskEl= function(taskDataObj){
+    var listItemEl=document.createElement("li");
+    listItemEl.className="task-item";
+    var taskInfoEl=document.createElement("div");
+    taskInfoEl.className="task-info";
+ 
+    taskInfoEl.innerHTML = "<h3 class='task-name'>" + taskDataObj.name + "</h3><span class='task-type'>" + taskDataObj.type + "</span>";
+    listItemEl.appendChild(taskInfoEl);
+    tasksToDoEl.appendChild(listItemEl);
+}
+
+formEl.addEventListener("submit", taskFormHandler);
+
